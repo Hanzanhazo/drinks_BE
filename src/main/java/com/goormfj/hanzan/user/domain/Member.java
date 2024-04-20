9e2613081b9a838e01528c892169355afd2ccba6
a1dotls <meeae9408@gmail.com>
2024-04-20 18:01:04 +0900

    feat: user domain

---
+++ src/main/java/com/goormfj/hanzan/user/domain/Member.java
@@ -0,0 +1,13 @@
+package com.goormfj.hanzan.user.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
