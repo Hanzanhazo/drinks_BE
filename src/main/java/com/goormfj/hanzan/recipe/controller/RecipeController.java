3337b9957719acf3f95f3aefc8beed13701a4cc8
jonghyeon <jonghyeon9398@gmail.com>
2024-05-03 16:51:03 +0900

    update

--- src/main/java/com/goormfj/hanzan/recipe/controller/RecipeController.java
+++ src/main/java/com/goormfj/hanzan/recipe/controller/RecipeController.java
@@ -10,9 +10,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/recipes")
 public class RecipeController {
     @Autowired
