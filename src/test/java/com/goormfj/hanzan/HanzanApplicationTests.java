ec2ed31a49710a38e77f5faa0a946d6ecd887d05
haystacks18 <103981340+haystacks18@users.noreply.github.com>
2024-05-16 13:02:43 +0900

    설정 조정 후 도커 이미지 로컬테스트 통과

--- src/test/java/com/goormfj/hanzan/HanzanApplicationTests.java
+++ src/test/java/com/goormfj/hanzan/HanzanApplicationTests.java
@@ -1,13 +1,8 @@
 package com.goormfj.hanzan;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HanzanApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
